0f236453ec3ad9c701a575a20ec65e1f6aae1970
Mateja <mail@matejamaric.com>
2024-04-01 21:24:39 +0200

    patch(server/blog): derivation improvements

--- hosts/server/blog/default.nix
+++ hosts/server/blog/default.nix
@@ -1,19 +1,18 @@
-let
-  pkgs = import <nixpkgs> { };
-in pkgs.stdenv.mkDerivation {
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.stdenv.mkDerivation {
   name = "blog";
   src = pkgs.fetchFromGitHub {
     owner = "MatejaMaric";
     repo = "blog";
     rev = "bb7193cf1f1b2e1662150400164b0b07e646b4a5";
-    sha256 = null;
+    hash = "sha256-MAOa7pHEIPijRulc0oz+KCd6dZ1WgShEmnrX1isuHBU=";
   };
   buildInputs = [ pkgs.hugo ];
   buildPhase = ''
     hugo
   '';
   installPhase = ''
-    mkdir -p $out/var/www/matejamaric.com
+    mkdir -p $out/var/www
     cp -r public $out/var/www/matejamaric.com
   '';
 }
