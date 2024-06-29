0dd12f3524ed2aaba1d043c5d434772b914005e9
Mateja <mail@matejamaric.com>
2024-06-29 11:46:12 +0200

    fix(home-manager)

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -3,21 +3,21 @@
   home.packages = [];
   home.sessionVariables = {
     EDITOR = "nvim";
+    PS1 = "\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ ";
   };
-
 
   programs = {
 
     bash = {
       enable = true;
       shellAliases = {
-        yta   = "yt-dlp --extract-audio -o '%(playlist_index)d-%(title)s.%(ext)s'"
-        ytv   = "yt-dlp -o '%(playlist_index)d-%(title)s.%(ext)s'"
-        ytv3  = "yt-dlp --write-auto-sub -f 'bestvideo[height<=360]+bestaudio/best[height<=360]' -o '%(playlist_index)d-%(title)s.%(ext)s'"
-        ytv4  = "yt-dlp --write-auto-sub -f 'bestvideo[height<=480]+bestaudio/best[height<=480]' -o '%(playlist_index)d-%(title)s.%(ext)s'"
-        ytv7  = "yt-dlp --write-auto-sub -f 'bestvideo[height<=720]+bestaudio/best[height<=720]' -o '%(playlist_index)d-%(title)s.%(ext)s'"
-        ytv10 = "yt-dlp --write-auto-sub -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' -o '%(playlist_index)d-%(title)s.%(ext)s'"
-        yts   = "mpv --ytdl-format='bestvideo[height<=360]+bestaudio'"
+        yta   = "yt-dlp --extract-audio -o '%(playlist_index)d-%(title)s.%(ext)s'";
+        ytv   = "yt-dlp -o '%(playlist_index)d-%(title)s.%(ext)s'";
+        ytv3  = "yt-dlp --write-auto-sub -f 'bestvideo[height<=360]+bestaudio/best[height<=360]' -o '%(playlist_index)d-%(title)s.%(ext)s'";
+        ytv4  = "yt-dlp --write-auto-sub -f 'bestvideo[height<=480]+bestaudio/best[height<=480]' -o '%(playlist_index)d-%(title)s.%(ext)s'";
+        ytv7  = "yt-dlp --write-auto-sub -f 'bestvideo[height<=720]+bestaudio/best[height<=720]' -o '%(playlist_index)d-%(title)s.%(ext)s'";
+        ytv10 = "yt-dlp --write-auto-sub -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' -o '%(playlist_index)d-%(title)s.%(ext)s'";
+        yts   = "mpv --ytdl-format='bestvideo[height<=360]+bestaudio'";
       };
     };
 
