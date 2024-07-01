57c415d9a69c2243740a049b3e00b420c1bef5f3
Mateja <mail@matejamaric.com>
2024-07-01 16:46:19 +0200

    wip: non working override of LunarVim package

--- hosts/macos/default.nix
+++ hosts/macos/default.nix
@@ -3,6 +3,17 @@ let
   system = "aarch64-darwin";
   nixpkgsConfig = {
     inherit system;
+    overlays = [(final: prev: {
+      lunarvim = prev.lunarvim.overrideAttrs (finalAttrs: previousAttrs: {
+        version = "latest";
+        src = prev.fetchFromGitHub {
+          owner = "LunarVim";
+          repo = "LunarVim";
+          rev = "85ccca97acfea9a465e354e18bb2f6109ba417f8";
+          hash = "sha256-lFX9vmNwbpd4dPpILS4B6Qea5OtqUd7ecaRLEzlkK3g=";
+        };
+      });
+    })];
   };
 in darwin.lib.darwinSystem {
   inherit system;
