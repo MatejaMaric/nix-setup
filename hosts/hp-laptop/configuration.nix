466ca6b95eaba320e0cd3ea8080dd6c74df1a791
Mateja <mail@matejamaric.com>
2024-07-14 20:32:55 +0200

    refactor(hp-laptop): remove unneeded comments

--- hosts/hp-laptop/configuration.nix
+++ hosts/hp-laptop/configuration.nix
@@ -1,7 +1,5 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
+# See available options: man 5 configuration.nix
+# Open NixOS manual: nixos-help
 { config, pkgs, pkgs-r2311, pkgs-unstable, ... }:
 let
   defaultPkgs = import ../../common/packages;
