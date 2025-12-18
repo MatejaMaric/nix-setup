dc863fcbe9f3a94df9daa02be9ff54c711dca668
Mateja <mail@matejamaric.com>
2025-12-18 12:13:45 +0100

    chore(flake): upgrade to 25.11

--- flake.nix
+++ flake.nix
@@ -2,17 +2,17 @@
   description = "Nix Flake for configuring systems I use";
 
   inputs = {
-    nixpkgs.url = "github:/NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:/NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:/NixOS/nixpkgs/nixpkgs-unstable";
 
-    nixpkgs-darwin.url = "github:/NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nixpkgs-darwin.url = "github:/NixOS/nixpkgs/nixpkgs-25.11-darwin";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-    darwin.url = "github:/lnl7/nix-darwin/nix-darwin-25.05";
+    darwin.url = "github:/lnl7/nix-darwin/nix-darwin-25.11";
     darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
 
-    home-manager.url = "github:/nix-community/home-manager/release-25.05";
+    home-manager.url = "github:/nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     disko.url = "github:nix-community/disko";
